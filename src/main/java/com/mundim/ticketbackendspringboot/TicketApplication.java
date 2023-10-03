217b13a15e27a1eacfc4c72203c40a788e6d91c8
Igor Mundim <mundim.github@gmail.com>
2023-10-03 11:28:59 -0300

    chore: start project

---
+++ src/main/java/com/mundim/ticketbackendspringboot/TicketApplication.java
@@ -0,0 +1,13 @@
+package com.mundim.ticketbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mundim/ticketbackendspringboot/TestTicketApplication.java
@@ -0,0 +1,23 @@
+package com.mundim.ticketbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestTicketApplication {
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(TicketApplication::main).with(TestTicketApplication.class).run(args);
+	}
+
+}
